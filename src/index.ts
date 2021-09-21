1d767a5de079543a5fea50510a4d103a1f4933f9
GHOST <ghostdevbusiness@gmail.com>
2021-09-21 10:45:49 +0100

    feat: write in typescript

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import type { ObfuscatorOptions } from 'javascript-obfuscator';
 import { obfuscate } from 'javascript-obfuscator';
+import type { Plugin } from 'rollup';
 
-export default function obfuscator(options = {}) {
+export default function obfuscator(options: ObfuscatorOptions = {}): Plugin {
     return {
         name: 'obfuscator',
 
@@ -9,6 +11,7 @@ export default function obfuscator(options = {}) {
             
             return {
                 code: obfuscated.getObfuscatedCode(),
+                
                 map:
                     options.sourceMap && options.sourceMapMode != 'inline'
                         ? obfuscated.getSourceMap()
