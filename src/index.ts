36c1d31d69585ac6522191443072e8223d066ec8
GHOST <ghostdevbusiness@gmail.com>
2021-09-21 10:50:44 +0100

    fix: expor

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import type { ObfuscatorOptions } from 'javascript-obfuscator';
 import { obfuscate } from 'javascript-obfuscator';
 import type { Plugin } from 'rollup';
 
-export default function obfuscator(options: ObfuscatorOptions = {}): Plugin {
+export function obfuscator(options: ObfuscatorOptions = {}): Plugin {
     return {
         name: 'obfuscator',
 
