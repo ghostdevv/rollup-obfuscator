2de7e44675f4fc0808e1a8b4e27cf817e43494b8
GHOST <ghostdevbusiness@gmail.com>
2023-08-23 02:11:12 +0000

    fix: target

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
 import type { Options } from 'tsup';
 
 export const tsup: Options = {
-    splitting: false,
-    sourcemap: false,
-    clean: true,
-    dts: true,
-    keepNames: true,
-    target: 'esnext',
-    format: ['esm', 'cjs'],
-    entryPoints: ['src/index.ts'],
+	splitting: false,
+	sourcemap: false,
+	clean: true,
+	dts: true,
+	keepNames: true,
+	target: 'es2021',
+	format: ['esm', 'cjs'],
+	entryPoints: ['src/index.ts'],
 };
