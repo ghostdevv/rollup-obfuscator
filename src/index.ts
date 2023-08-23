196ff0d7d352d3914ab6fce8c57b940d47e3cbda
GHOST <ghostdevbusiness@gmail.com>
2023-08-23 01:55:44 +0000

    feat: remove options.global and cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,73 +4,49 @@ import { createFilter } from '@rollup/pluginutils';
 import Obfuscator from 'javascript-obfuscator';
 import type { Plugin } from 'rollup';
 
-interface ObfuscatorPluginOptions extends ObfuscatorOptions {
-    /**
-     * It's recommended to keep this enabled, since it might cause issues. However if you need access to `include` and `exclude` options you can disable this
-     * @default true
-     */
-    global?: boolean;
-
+export interface RollupObfuscatorOptions extends ObfuscatorOptions {
     /**
      * A [FilterPattern](https://github.com/rollup/plugins/blob/master/packages/pluginutils/types/index.d.ts#L23) of files to include. By default only allows js/ts files.
      * Only works with global set to false
+     * @default ['**\/*.js', '**\/*.ts']
      */
     include?: FilterPattern;
 
     /**
      * A [FilterPattern](https://github.com/rollup/plugins/blob/master/packages/pluginutils/types/index.d.ts#L23) of files to exclude. By default ignores node_modules.
      * Only works with global set to false
+     * @default ['node_modules\/**']
      */
     exclude?: FilterPattern;
 }
 
-export function obfuscator(
-    options: ObfuscatorPluginOptions = { global: true },
-): Plugin {
-    const filter = createFilter(
-        options.include || ['**/*.js', '**/*.ts'],
-        options.exclude || ['node_modules/**'],
-    );
+export function obfuscator(options: RollupObfuscatorOptions = {}): Plugin {
+    const {
+        include = ['**/*.js', '**/*.ts'],
+        exclude = ['node_modules/**'],
+        sourceMap = true,
+        ...obfuscatorOptions
+    } = options;
 
-    if (options.global != true && options.global != false) {
-        options.global = true;
-    }
+    const filter = createFilter(include, exclude);
 
     return {
         name: 'obfuscator',
 
-        transform: options.global
-            ? undefined
-            : (code, id) => {
-                  if (!filter(id)) return null;
+        transform(code, id) {
+            if (!filter(id)) return null;
 
-                  const obfuscated = Obfuscator.obfuscate(code, {
-                      ...options,
-                      inputFileName: id,
-                  });
+            const result = Obfuscator.obfuscate(code, {
+                stringArray: false,
+                ...obfuscatorOptions,
+                inputFileName: id,
+                sourceMap,
+            });
 
-                  return {
-                      code: obfuscated.getObfuscatedCode(),
-                      map: options.sourceMap
-                          ? obfuscated.getSourceMap()
-                          : undefined,
-                  };
-              },
-
-        renderChunk: options.global
-            ? (code, { fileName }) => {
-                  const obfuscated = Obfuscator.obfuscate(code, {
-                      ...options,
-                      inputFileName: fileName,
-                  });
-
-                  return {
-                      code: obfuscated.getObfuscatedCode(),
-                      map: options.sourceMap
-                          ? obfuscated.getSourceMap()
-                          : undefined,
-                  };
-              }
-            : undefined,
+            return {
+                code: result.getObfuscatedCode(),
+                map: sourceMap ? result.getSourceMap() : undefined,
+            };
+        },
     };
 }
