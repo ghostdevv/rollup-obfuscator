900d7505dff13ed7c3e8dbcc99138a5e95566aa6
GHOST <ghostdevbusiness@gmail.com>
2023-08-23 02:06:30 +0000

    chore: update prettier config

--- src/index.ts
+++ src/index.ts
@@ -5,51 +5,51 @@ import Obfuscator from 'javascript-obfuscator';
 import type { Plugin } from 'rollup';
 
 export interface RollupObfuscatorOptions extends ObfuscatorOptions {
-    /**
-     * A [FilterPattern](https://github.com/rollup/plugins/blob/master/packages/pluginutils/types/index.d.ts#L23) of files to include. By default only allows js/ts files.
-     * @default ['**\/*.js', '**\/*.ts']
-     */
-    include?: FilterPattern;
+	/**
+	 * A [FilterPattern](https://github.com/rollup/plugins/blob/master/packages/pluginutils/types/index.d.ts#L23) of files to include. By default only allows js/ts files.
+	 * @default ['**\/*.js', '**\/*.ts']
+	 */
+	include?: FilterPattern;
 
-    /**
-     * A [FilterPattern](https://github.com/rollup/plugins/blob/master/packages/pluginutils/types/index.d.ts#L23) of files to exclude. By default ignores node_modules.
-     * @default ['node_modules\/**']
-     */
-    exclude?: FilterPattern;
+	/**
+	 * A [FilterPattern](https://github.com/rollup/plugins/blob/master/packages/pluginutils/types/index.d.ts#L23) of files to exclude. By default ignores node_modules.
+	 * @default ['node_modules\/**']
+	 */
+	exclude?: FilterPattern;
 }
 
 export function obfuscator(options: RollupObfuscatorOptions = {}): Plugin {
-    const {
-        include = ['**/*.js', '**/*.ts'],
-        exclude = ['node_modules/**'],
-        sourceMap = true,
-        ...obfuscatorOptions
-    } = options;
+	const {
+		include = ['**/*.js', '**/*.ts'],
+		exclude = ['node_modules/**'],
+		sourceMap = true,
+		...obfuscatorOptions
+	} = options;
 
-    const filter = createFilter(include, exclude);
+	const filter = createFilter(include, exclude);
 
-    return {
-        name: 'obfuscator',
+	return {
+		name: 'obfuscator',
 
-        transform(code, id) {
-            if (!filter(id)) {
-                this?.debug(`[rollup-obfuscator] Ignoring "${id}"`);
-                return null;
-            }
+		transform(code, id) {
+			if (!filter(id)) {
+				this?.debug(`[rollup-obfuscator] Ignoring "${id}"`);
+				return null;
+			}
 
-            this?.debug(`[rollup-obfuscator] Obfuscating "${id}"`);
+			this?.debug(`[rollup-obfuscator] Obfuscating "${id}"`);
 
-            const result = Obfuscator.obfuscate(code, {
-                stringArray: false,
-                ...obfuscatorOptions,
-                inputFileName: id,
-                sourceMap,
-            });
+			const result = Obfuscator.obfuscate(code, {
+				stringArray: false,
+				...obfuscatorOptions,
+				inputFileName: id,
+				sourceMap,
+			});
 
-            return {
-                code: result.getObfuscatedCode(),
-                map: sourceMap ? result.getSourceMap() : undefined,
-            };
-        },
-    };
+			return {
+				code: result.getObfuscatedCode(),
+				map: sourceMap ? result.getSourceMap() : undefined,
+			};
+		},
+	};
 }
